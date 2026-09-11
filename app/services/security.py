0c12491426efce0bfa78c0255d709ec58d6f48ef
Viveknathtiwari01 <viveknathtiwari01@gmail.com>
2026-09-11 20:18:45 +0530

    feat: Update password hashing schemes to include bcrypt in security service and requirements

--- app/services/security.py
+++ app/services/security.py
@@ -5,7 +5,7 @@ from jose import jwt, JWTError
 from passlib.context import CryptContext
 from app.core.config import settings
 
-pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
+pwd_context = CryptContext(schemes=["bcrypt", "argon2"], deprecated="auto")
 
 class SecurityService:
     @staticmethod
